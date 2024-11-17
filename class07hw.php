4e18761928eae868c36acddf51cc03de0a4ad8ca
Muhammad Imran Hosen Opu <muhammadimranhosenopu@gmail.com>
2024-11-17 10:12:40 +0600

    Add files via upload

---
+++ class07hw.php
@@ -0,0 +1,16 @@
+<?php
+$user_name = 'saif';
+$password = 12345678;
+
+if($user_name === 'opu'){
+    echo 'your username is correct';
+}else{
+    echo 'your username is wrong';
+}
+echo '<br/>';
+if($password === 12345678){
+    echo 'your password is correct';
+}else{
+    echo 'your pasword is wrong';
+}
+?>
